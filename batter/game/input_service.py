a404a33d97776c98f39b25b92dd75f5b5eddebf3
DexterDavenport <91489096+DexterDavenport@users.noreply.github.com>
2021-11-18 17:30:12 -0700

    asdw keys work as controller now

--- batter/game/input_service.py
+++ batter/game/input_service.py
@@ -31,17 +31,18 @@ class InputService:
         dx = 0
         dy = 0
 
-        if self.is_left_pressed():
+        if self.is_left_pressed() or self.is_a_pressed():
             dx = -1
         
-        if self.is_right_pressed():
+        if self.is_right_pressed() or self.is_d_pressed():
             dx = 1
         
-        # if self.is_up_pressed():
-        #     dy = -1
+        if self.is_up_pressed() or self.is_w_pressed():
+            dy = -1
         
-        # if self.is_down_pressed():
-        #     dy = 1
+        if self.is_down_pressed() or self.is_s_pressed():
+            dy = 1
+        
 
         direction = Point(dx, dy)
         return direction
@@ -52,11 +53,27 @@ class InputService:
     def is_right_pressed(self):
         return raylibpy.is_key_down(raylibpy.KEY_RIGHT)
 
-    # def is_up_pressed(self):
-    #     return raylibpy.is_key_down(raylibpy.KEY_UP)
+    def is_up_pressed(self):
+        return raylibpy.is_key_down(raylibpy.KEY_UP)
 
-    # def is_down_pressed(self):
-    #     return raylibpy.is_key_down(raylibpy.KEY_DOWN)
+    def is_down_pressed(self):
+        return raylibpy.is_key_down(raylibpy.KEY_DOWN)
+    
+
+
+    def is_a_pressed(self):
+        return raylibpy.is_key_down(raylibpy.KEY_A)
+
+    def is_d_pressed(self):
+        return raylibpy.is_key_down(raylibpy.KEY_D)
+
+    def is_w_pressed(self):
+        return raylibpy.is_key_down(raylibpy.KEY_W)
+
+    def is_s_pressed(self):
+        return raylibpy.is_key_down(raylibpy.KEY_S)
+
+
 
     def window_should_close(self):
         return raylibpy.window_should_close()
